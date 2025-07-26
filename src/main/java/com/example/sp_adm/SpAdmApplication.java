1f4b2e529de1728d56b1d64b2a73a028b20b8add
NAGARJUNA <nagarjunanani333@gmail.com>
2025-07-26 14:29:26 +0530

    Started a repo for new api

---
+++ src/main/java/com/example/sp_adm/SpAdmApplication.java
@@ -0,0 +1,13 @@
+package com.example.sp_adm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpAdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpAdmApplication.class, args);
+	}
+
+}
